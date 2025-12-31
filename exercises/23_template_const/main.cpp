68b6f739f4d5dc7fa06b48393d7b951f14517a80
doom2099 <1274754986@qq.com>
2025-12-31 23:00:10 +0800

    30

--- exercises/23_template_const/main.cpp
+++ exercises/23_template_const/main.cpp
@@ -11,6 +11,10 @@ struct Tensor {
     Tensor(unsigned int const shape_[N]) {
         unsigned int size = 1;
         // TODO: 填入正确的 shape 并计算 size
+        for(unsigned int i=0;i<N;i++){
+            size*=shape_[i];
+        }
+        memcpy(shape,shape_,N*sizeof(unsigned int));
         data = new T[size];
         std::memset(data, 0, size * sizeof(T));
     }
@@ -31,10 +35,12 @@ struct Tensor {
 
 private:
     unsigned int data_index(unsigned int const indices[N]) const {
-        unsigned int index = 0;
-        for (unsigned int i = 0; i < N; ++i) {
+        unsigned int index = indices[0];
+        for (unsigned int i = 1; i < N; ++i) {
             ASSERT(indices[i] < shape[i], "Invalid index");
             // TODO: 计算 index
+            index=index*shape[i]+indices[i];
+
         }
         return index;
     }
